ccde51222bfe9174ca100b054c2bb5e7c9c66f3c
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-11-02 14:44:33 -0800

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("HELLO WORLD");
+import axios from 'axios';
+
+import { logger } from './logger';
+
+const fileUrl = 'https://ironclad.design/assets/logo/ironclad-logo.svg';
+
+const main = () => {
+  logger.debug('--- START ---');
+  logger.info(fileUrl);
+  logger.debug('---  END  ---');
+};
+
+main();
