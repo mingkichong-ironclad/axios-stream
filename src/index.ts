c00ec543050049ef7ad34e799c5ca0a41a27057f
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-11-02 17:52:55 -0800

    use strea to promise to read response data stream

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as fs from 'fs';
+const streamToPromise = require('stream-to-promise');
 
 import { logger } from './logger';
 
@@ -7,6 +8,8 @@ const fileUrl = 'https://ironcladapp.com/wp-content/uploads/2019/10/Ironclad_log
 const requestConfig = { responseType: 'stream' as const }; // so data is a readable stream
 
 const output = fs.createWriteStream('test.png');
+
+const input = fs.createReadStream('package.json');
 
 const main = async () => {
   logger.debug('--- START ---');
@@ -18,14 +21,19 @@ const main = async () => {
     logger.info(JSON.stringify(headers, null, 2));
     logger.info(JSON.stringify(config, null, 2));
 
-    const data: NodeJS.ReadableStream = response.data;
-    let sum = 0;
-    data.on('data', (chunk: any) => {
-      console.log(`Received ${chunk.length} bytes of data.`);
-      sum += chunk.length;
-      console.log(`total so far: ${sum}`);
-      output.write(chunk);
-    });
+    const data = await streamToPromise(response.data);
+    logger.debug(`Received ${data.length} bytes of data.`);
+    output.write(data);
+    output.close();
+
+    // const data: NodeJS.ReadableStream = response.data;
+    // let sum = 0;
+    // data.on('data', (chunk: any) => {
+    //   console.log(`Received ${chunk.length} bytes of data.`);
+    //   sum += chunk.length;
+    //   console.log(`total so far: ${sum}`);
+    //   output.write(chunk);
+    // });
   } catch (err) {
     logger.error('ERR', err);
   }
